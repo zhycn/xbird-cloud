d320d1e3ce5745cd267dc0773b47a5d3d2b7a6b8
zhycn <qzhservice@gmail.com>
2018-06-23 14:50:02 +0800

    Add Spring Cloud Bus & OpenFeign commit.

---
+++ xbird-cloud-openfeign/src/test/java/com/xbird/cloud/openfeign/OpenFeignApplicationTests.java
@@ -0,0 +1,15 @@
+package com.xbird.cloud.openfeign;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpenFeignApplicationTests {
+
+  @Test
+  public void contextLoads() {}
+
+}
