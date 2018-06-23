1d7b01be8fcb6279538f0f237c7bd5d1e7e716f4
zhycn <qzhservice@gmail.com>
2018-06-23 20:26:48 +0800

    Update Spring Cloud Config commit.

--- xbird-cloud-config/xbird-cloud-config-client/src/main/java/com/xbird/cloud/config/client/ConfigClientApplication.java
+++ xbird-cloud-config/xbird-cloud-config-client/src/main/java/com/xbird/cloud/config/client/ConfigClientApplication.java
@@ -1,12 +1,26 @@
 package com.xbird.cloud.config.client;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class ConfigClientApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ConfigClientApplication.class, args);
+  }
+
+  @Value("${foo}")
+  private String foo;
+
+  @GetMapping("/foo")
+  public String hello() {
+    return foo;
   }
 }
