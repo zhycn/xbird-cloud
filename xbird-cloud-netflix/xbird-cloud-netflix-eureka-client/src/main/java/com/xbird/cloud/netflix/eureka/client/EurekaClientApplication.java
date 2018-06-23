bc98df0fac114cbc06f5880375a604c8100cd6f8
zhycn <qzhservice@gmail.com>
2018-06-23 19:20:38 +0800

    Add Eureka commit.

--- xbird-cloud-netflix/xbird-cloud-netflix-eureka-client/src/main/java/com/xbird/cloud/netflix/eureka/client/EurekaClientApplication.java
+++ xbird-cloud-netflix/xbird-cloud-netflix-eureka-client/src/main/java/com/xbird/cloud/netflix/eureka/client/EurekaClientApplication.java
@@ -2,11 +2,22 @@ package com.xbird.cloud.netflix.eureka.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
+@RestController
 public class EurekaClientApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(EurekaClientApplication.class, args);
+  }
+
+  @GetMapping("/")
+  public String home(@RequestParam(defaultValue = "World") String name) {
+    return "Hello " + name;
   }
 }
--- xbird-cloud-netflix/xbird-cloud-netflix-eureka-server/src/main/java/com/xbird/cloud/netflix/eureka/server/EurekaServerApplication.java
+++ xbird-cloud-netflix/xbird-cloud-netflix-eureka-server/src/main/java/com/xbird/cloud/netflix/eureka/server/EurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.xbird.cloud.netflix.eureka.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // run a Eureka Server
 public class EurekaServerApplication {
 
   public static void main(String[] args) {
