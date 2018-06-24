fe27c9c719a42dfd6d34258a5c7ef070dc8b7207
zhycn <qzhservice@gmail.com>
2018-06-24 09:40:57 +0800

    Initial commit.

---
+++ xbird-cloud-vault/src/test/java/com/xbird/cloud/vault/VaultApplicationTests.java
@@ -0,0 +1,15 @@
+package com.xbird.cloud.vault;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VaultApplicationTests {
+
+  @Test
+  public void contextLoads() {}
+
+}
