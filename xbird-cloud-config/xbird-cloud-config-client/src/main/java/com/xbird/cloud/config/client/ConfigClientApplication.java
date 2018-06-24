fa96e8341198edc00f0b8020e4152c14c3e92c73
zhycn <qzhservice@gmail.com>
2018-06-24 15:08:27 +0800

    Update Config commit.

--- xbird-cloud-config/xbird-cloud-config-client/src/main/java/com/xbird/cloud/config/client/ConfigClientApplication.java
+++ xbird-cloud-config/xbird-cloud-config-client/src/main/java/com/xbird/cloud/config/client/ConfigClientApplication.java
@@ -2,13 +2,11 @@ package com.xbird.cloud.config.client;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@EnableEurekaClient
+@SpringCloudApplication
 @RestController
 public class ConfigClientApplication {
 
--- xbird-cloud-config/xbird-cloud-config-server/src/main/java/com/xbird/cloud/config/server/ConfigServerApplication.java
+++ xbird-cloud-config/xbird-cloud-config-server/src/main/java/com/xbird/cloud/config/server/ConfigServerApplication.java
@@ -1,16 +1,17 @@
 package com.xbird.cloud.config.server;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
-@SpringBootApplication
+@SpringCloudApplication
+@EnableHystrixDashboard
 @EnableConfigServer
-@EnableEurekaClient
 public class ConfigServerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ConfigServerApplication.class, args);
   }
+
 }
